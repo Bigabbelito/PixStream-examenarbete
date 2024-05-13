606f6a8fa79ba240437fb5212be6ba0d23ea9238
Abdulah Abou Shihab <118314337+Bigabbelito@users.noreply.github.com>
2024-05-13 14:56:12 +0200

    appen är nästan redo

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,5 @@
 import { Box, Button, Flex, Link, Tooltip } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
-import { InstagramLogo, InstagramMobileLogo } from "../../assets/constants";
 
 import { BiLogOut } from "react-icons/bi";
 import useLogout from "../../hooks/useLogout";
@@ -21,7 +20,7 @@ const Sidebar = () => {
 		>
 			<Flex direction={"column"} gap={10} w='full' height={"full"}>
 				<Link to={"/"} as={RouterLink} pl={2} display={{ base: "none", md: "block" }} cursor='pointer'>
-					<InstagramLogo />
+					
 				</Link>
 				<Link
 					to={"/"}
@@ -35,7 +34,7 @@ const Sidebar = () => {
 					w={10}
 					cursor='pointer'
 				>
-					<InstagramMobileLogo />
+					
 				</Link>
 				<Flex direction={"column"} gap={5} cursor={"pointer"}>
 					<SidebarItems />
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -6,12 +6,7 @@ const AuthPage = () => {
 		<Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
 			<Container maxW={"container.md"} padding={0}>
 				<Flex justifyContent={"center"} alignItems={"center"} gap={10}>
-					{/* Left hand-side */}
-					<Box display={{ base: "none", md: "block" }}>
-						<Image src='/auth.png' h={650} alt='Phone img' />
-					</Box>
-
-					{/* Right hand-side */}
+					
 					<VStack spacing={4} align={"stretch"}>
 						<AuthForm />
 						<Box textAlign={"center"}>Get the app.</Box>
